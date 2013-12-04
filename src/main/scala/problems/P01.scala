b933fd450dbe25aaf60aaef470706b9111435787
Andrew Gray <andrew.gray@hootsuite.com>
2013-12-04 15:00:36 -0800

    P01

--- src/main/scala/problems/P01.scala
+++ src/main/scala/problems/P01.scala
@@ -1,5 +1,23 @@
 package problems
 
+/*
+ * Find the last element of a list.
+Example:
+scala> last(List(1, 1, 2, 3, 5, 8))
+res0: Int = 8
+ */
 object P01 {
+  
+  // assumes list has at least one element
+  def lastElement(l:List[Int]):Int = {
+    
+    // be sure to spell Nil correctly...
+    l match {
+      case elem :: Nil => elem
+      case elem :: rest => lastElement(rest)
+      case _ => throw new Exception("burp")
+    }
+    
+  }
 
 }
