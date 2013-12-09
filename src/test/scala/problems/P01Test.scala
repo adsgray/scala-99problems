8f690c0d36421173680e992e97ac218f1112501f
Andrew Gray <andrew.gray@hootsuite.com>
2013-12-09 13:41:08 -0800

    tests for penultimate

--- src/test/scala/problems/P01Test.scala
+++ src/test/scala/problems/P01Test.scala
@@ -22,5 +22,28 @@ class P01Test extends WordSpec with Matchers {
   }
   
   
+  "penultimate" should {
+    "return penultimate element of a list of 2" in {
+      val l = List(1,2)
+      P02.penultimate(l) shouldEqual 1
+    }
+    
+    "return penultimate element of a list > 2" in {
+      val l = List(1,2,3,4,5)
+      P02.penultimate(l) shouldEqual 4
+    }
+    
+    "throw an exception for an empty list" in {
+      val l = List()
+      an [Exception] should be thrownBy P02.penultimate(l)
+    }
+    
+    "throw an exception for a list of 1?" in {
+      val l = List(42)
+      an [Exception] should be thrownBy P02.penultimate(l)
+    }
+    
+  }
+  
   
 }
